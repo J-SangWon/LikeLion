220481071f82bf2289b69841af31267c37999b09
J-SangWon <124190818+J-SangWon@users.noreply.github.com>
2025-02-21 10:22:54 +0900

    Study02

---
+++ LikeLion02/LikeLion02/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLion02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = 2025;
+
+            Console.WriteLine("이번 년도는 " + year + "년 입니다");
+        }
+    }
+}
