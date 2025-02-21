33429ee87d96167bdeafaa95e46517421a3bccea
J-SangWon <124190818+J-SangWon@users.noreply.github.com>
2025-02-21 10:40:26 +0900

    study03

---
+++ LikeLion03/LikeLion03/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLion03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //리터럴 : 코드에서 고정된 값을 의미한다.
+            int hp = 100;
+            double att = 56.7;
+            char grade = 'S';
+            String name = "테스트 캐릭";
+
+            Console.WriteLine(name);
+            Console.WriteLine(grade);
+            Console.WriteLine(hp);
+            Console.WriteLine(att);
+        }
+    }
+}
